2f8504962b8faa2ed2d0414c9934c77300ab68a8
tengufromsky <nick27surgut@gmail.com>
2019-07-21 21:59:38 +0300

    try new hack for evading optomizations

--- performance/slice_bench_test.go
+++ performance/slice_bench_test.go
@@ -2,67 +2,85 @@ package performance
 
 import "testing"
 
+var benchmarkDynamicLen_GetLenMediumListenchVar int
+
 func BenchmarkDynamicLen_GetLenMediumList(b *testing.B) {
 	ll := dynamicLen{
 		items: mediumList,
 	}
+	var res int
 	for i := 0; i < b.N; i++ {
-		res := ll.GetLen()
-		_ = res
+		res = ll.GetLen()
 	}
+	benchmarkDynamicLen_GetLenMediumListenchVar = res
 }
+
+var benchmarkLockedLen_GetLenMediumListVar int
 
 func BenchmarkLockedLen_GetLenMediumList(b *testing.B) {
 	ll := lockedLen{
 		items:    mediumList,
 		itemsLen: 10,
 	}
+	var res int
 	for i := 0; i < b.N; i++ {
-		res := ll.GetLen()
-		_ = res
+		res = ll.GetLen()
 	}
+	benchmarkLockedLen_GetLenMediumListVar = res
 }
+
+var benchmarkLockedLen_GetLenShortListVar int
 
 func BenchmarkLockedLen_GetLenShortList(b *testing.B) {
 	ll := lockedLen{
 		items:    shortList,
 		itemsLen: 5,
 	}
+	var res int
 	for i := 0; i < b.N; i++ {
-		res := ll.GetLen()
-		_ = res
+		res = ll.GetLen()
 	}
+	benchmarkLockedLen_GetLenShortListVar = res
 }
+
+var benchmarkDynamicLen_GetLenShortListVar int
 
 func BenchmarkDynamicLen_GetLenShortList(b *testing.B) {
 	ll := dynamicLen{
 		items: shortList,
 	}
+	var res int
 	for i := 0; i < b.N; i++ {
-		res := ll.GetLen()
-		_ = res
+		res = ll.GetLen()
 	}
+	benchmarkDynamicLen_GetLenShortListVar = res
 }
+
+var benchmarkLockedLen_GetLenLongListVar int
 
 func BenchmarkLockedLen_GetLenLongList(b *testing.B) {
 	ll := lockedLen{
 		items:    bigList,
 		itemsLen: 100,
 	}
+	var res int
 	for i := 0; i < b.N; i++ {
-		res := ll.GetLen()
-		_ = res
+		res = ll.GetLen()
 	}
+	benchmarkLockedLen_GetLenLongListVar = res
 }
+
+var benchmarkDynamicLen_GetLenLongListVar int
 
 func BenchmarkDynamicLen_GetLenLongList(b *testing.B) {
 	ll := dynamicLen{
 		items: bigList,
 	}
+	var res int
 	for i := 0; i < b.N; i++ {
-		res := ll.GetLen()
-		_ = res
+		res = ll.GetLen()
 	}
+	benchmarkDynamicLen_GetLenLongListVar = res
 }
 
 var shortList = []int{1, 2, 3, 4, 5}
